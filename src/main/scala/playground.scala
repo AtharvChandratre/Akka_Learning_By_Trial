d451a0dcacc0c423097d3de2aa5f4cefba4def70
Atharv Chandratre <atharvchandratre@gmail.com>
2021-08-07 22:48:13 +0530

    Added a terminate statement at the end of the program

--- src/main/scala/playground.scala
+++ src/main/scala/playground.scala
@@ -3,16 +3,18 @@ import akka.actor.{Actor, ActorSystem, Props}
 object playground extends App {
 
   class ExampleActor extends Actor {
-    def receive = {
+    def receive: Receive = {
       case s: String => println("Received String: " + s)
       case i: Int => println("Received Number: " + i)
     }
   }
 
-    val system = ActorSystem("ExampleSystem");
+    val system = ActorSystem("ExampleSystem")
     val actor = system.actorOf(Props[ExampleActor],"ExampleActor1")
 
     actor!"Hi"
     actor!123
     actor!'c'
+
+  system.terminate()
 }
